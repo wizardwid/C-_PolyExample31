c727a6ce191c6aa2cfc64150831f804927a6ed07
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-06-11 10:12:04 +0900

    강아지, 고양이 관계 클래스 객체

---
+++ Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample31
+{
+    internal class Cat
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public Cat() { this.Age = 0; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+
+        public void Meow() { Console.WriteLine("냥냥 웁니다."); }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample31
+{
+    internal class Dog
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public Dog() { this.Age = 0; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+
+        public void Bark() { Console.WriteLine("와왕 짖습니다."); }
+    }
+}
